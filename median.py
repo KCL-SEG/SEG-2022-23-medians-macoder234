9bf35608aa6f5465572c74fe4056b3f98050e785
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-10-07 11:19:38 +0100

    test4

--- median.py
+++ median.py
@@ -32,6 +32,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
         checkEven()
     except ValueError:
         print("Some input could not be converted to a number!")
